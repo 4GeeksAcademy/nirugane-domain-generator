ede775e97fcad5dc3aa2762e3f7c3c59c27a1f0c
Nicolas Herrera Salazar <nherresk@gmail.com>
2025-07-05 03:58:22 +0000

    random extensions added

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let extensions = ['.net', '.gov.', '.com'];
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-            console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+            let randomExt = extensions[Math.floor(Math.random()*extensions.length)];
+
+            console.log(pronoun[i] + adj[j] + noun[k] + randomExt);
         }
     }
 }
